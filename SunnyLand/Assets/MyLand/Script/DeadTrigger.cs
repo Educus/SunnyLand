896c7d45eba6664e43ab226b5c49fa221ea02eec
Educus <kms369400@naver.com>
2023-11-20 03:46:22 +0900

    23.11.20

--- SunnyLand/Assets/MyLand/Script/DeadTrigger.cs
+++ SunnyLand/Assets/MyLand/Script/DeadTrigger.cs
@@ -6,12 +6,17 @@ using UnityEngine;
 public class DeadTrigger : MonoBehaviour
 {
     [SerializeField] GameObject target;
+    [SerializeField] Rigidbody2D rigid;
+    [SerializeField] int jumpPower;
     
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag("Player"))
         {
+            rigid = collision.GetComponent<Rigidbody2D>();
             Destroy(target);
+            rigid.velocity = new Vector3(rigid.velocity.x, 0);
+            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
         }
     }
 }
--- SunnyLand/Assets/MyLand/Script/Enemy.cs
+++ SunnyLand/Assets/MyLand/Script/Enemy.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField] Transform target;
-    [SerializeField] bool walk;
-    [SerializeField] bool fly;
     [SerializeField] float moveSpeed;
+    [SerializeField] bool walk;
+    [SerializeField] float cycle;
     [SerializeField] bool jump;
     [SerializeField] float jumpPower;
+    [SerializeField] bool fly;
     [SerializeField] Vector3[] position;
 
     SpriteRenderer spr;
+    Rigidbody2D rigid;
     Vector3 originPosition;
     bool isReverse;
     int index;
@@ -22,6 +25,7 @@ public class Enemy : MonoBehaviour
     private void Start()
     {
         spr = GetComponent<SpriteRenderer>();
+        rigid = GetComponent<Rigidbody2D>();
 
         originPosition = transform.position;
         index = 0;
@@ -29,7 +33,7 @@ public class Enemy : MonoBehaviour
     }
     private void Update()
     {
-        if (walk == true || fly == true)
+        if ((walk == true && cycle == 0)|| fly == true) // 사이클 없이 걷기 or 날기
         {
             transform.position = Vector3.MoveTowards(transform.position, destination, moveSpeed * Time.deltaTime);
             if (transform.position == destination)
@@ -41,9 +45,36 @@ public class Enemy : MonoBehaviour
             if (fly == true)
             {
                 // fly몹일때 플레이어 위치마다 방향 전환
-                if ((transform.position.x - target.position.x) > 0) spr.flipX = false;
-                else if ((transform.position.x - target.position.x) < 0) spr.flipX = true;
+                filp(new Vector2(target.position.x, 0));
             }
+        }
+        else if(walk == true || jump == true)
+        {
+            
+            Debug.Log(isReverse);
+            if (jump == true)
+            {
+            
+            }
+            else if(walk == true)
+            {
+                //rigid.velocity = new Vector2(moveSpeed/*뭔데 슈바*/,rigid.velocity.y);
+                rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            }
+            if(isReverse ? transform.position.x >= destination.x : transform.position.x <= destination.x)
+            {
+                index += (isReverse ? -1 : 1);
+                if (index == (isReverse ? 0 : position.Length - 1)) isReverse = !isReverse;
+            }
+
+            filp(destination);
+        }
+
+       
+        void filp(Vector2 nextTarget)
+        {
+            if ((nextTarget.x - transform.position.x) < 0) spr.flipX = false;
+            else if ((nextTarget.x - transform.position.x) > 0) spr.flipX = true;
         }
     }
 
@@ -62,4 +93,5 @@ public class Enemy : MonoBehaviour
             Gizmos.DrawLine(start, end);
         }
     }
+    
 }
