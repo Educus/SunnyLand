a49742218266fcceeb2a78c7a2897a22adec88fe
Educus <kms369400@naver.com>
2024-04-19 15:17:43 +0900

    Revert "24.03.13"
    This reverts commit 36c2ef398a797fca242ddf33d4680d6585a14d10.

--- SunnyLand/Assets/MyLand/Script/GameManager.cs
+++ SunnyLand/Assets/MyLand/Script/GameManager.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,7 +7,7 @@ public class GameManager : MonoBehaviour
 {
     public void OnGameClear()
     {
-        UIController.Instance.UIClear();   
+        
     }
 
     public void OnGameOver()
@@ -18,18 +17,6 @@ public class GameManager : MonoBehaviour
 
     private void OnLoadSence()
     {
-        if(UITMP.life > 0)
-        {
-            UITMP.life--;
-            UITMP.cherryCount = 0;
-            UITMP.gemCount = 0;
-            
-            SceneManager.LoadScene("SampleScene");
-        }
-        else
-        {
-            UIController.Instance.UIGameOver();
-        }
-        
+        SceneManager.LoadScene("SampleScene");
     }
 }
