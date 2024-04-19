d1cf9a250d424867d1380fa93ccd741d0a237913
Educus <kms369400@naver.com>
2024-04-19 15:20:58 +0900

    Reapply "24.04.08"
    This reverts commit 1a32e3f3e990977c915e11885decf79c7e0b65ba.

--- SunnyLand/Assets/MyLand/Script/CrankTrigger.cs
+++ SunnyLand/Assets/MyLand/Script/CrankTrigger.cs
@@ -1,14 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class CrankTrigger : MonoBehaviour
 {
     [SerializeField] UnityEvent CrankEvent;
+    [SerializeField] Transform BridgeMain;
+    [SerializeField] Transform bridge;
+
+    Animator anim;
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
+        if(anim.GetBool("TouchCrank") == false)
+        {
+            StartCoroutine(RotationBridge());
+        }
+        anim.SetBool("TouchCrank", true);
         
+    }
+    
+    IEnumerator RotationBridge()
+    {
+        Vector3 rotVec = Vector3.forward * 90;
+        bridge.position = BridgeMain.position;
+        bridge.Rotate(rotVec);
+        bridge.Translate(bridge.right * -4, Space.World);
+
+        yield return null;
     }
 }
