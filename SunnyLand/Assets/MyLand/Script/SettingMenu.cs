1b47696396707ec1a9401987d049a4cb08fe2ecd
Educus <kms369400@naver.com>
2024-05-14 20:36:12 +0900

    24.05.14 end 추가 없음

--- SunnyLand/Assets/MyLand/Script/SettingMenu.cs
+++ SunnyLand/Assets/MyLand/Script/SettingMenu.cs
@@ -4,18 +4,6 @@ using UnityEngine;
 
 public class SettingMenu : MonoBehaviour
 {
-    public void LeftKey()
-    {
-
-    }
-    public void RightKey() 
-    { 
-
-    }
-    public void JumpKey()
-    {
-
-    }
     public void BGMSound()
     {
 
